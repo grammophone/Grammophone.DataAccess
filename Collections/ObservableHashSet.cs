12d95fe62f405a17c282f2b8628ed7310f1ad2b2
grammophone <emmanuel.liossis@gmail.com>
2026-06-22 15:41:52 -0400

    Fix ICollectoin<T> and ISet<T> implementation for ObservableHashSet<T>.

--- Collections/ObservableHashSet.cs
+++ Collections/ObservableHashSet.cs
@@ -90,10 +90,22 @@ namespace Grammophone.DataAccess.Collections
 		public new bool Add(T item)
 		{
 			bool added = base.Add(item);
+
 			if (added)
+			{
 				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item));
+			}
 
 			return added;
+		}
+
+		/// <summary>
+		/// Adds an element to the set and raises the appropriate collection changed event.
+		/// </summary>
+		/// <param name="item">The item to add.</param>
+		void ICollection<T>.Add(T item)
+		{
+			Add(item);
 		}
 
 		/// <summary>
@@ -104,10 +116,23 @@ namespace Grammophone.DataAccess.Collections
 		public new bool Remove(T item)
 		{
 			bool removed = base.Remove(item);
+
 			if (removed)
+			{
 				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item));
+			}
 
 			return removed;
+		}
+
+		/// <summary>
+		/// Removes an element from the set and raises the appropriate collection changed event.
+		/// </summary>
+		/// <param name="item">The item to remove.</param>
+		/// <returns>true if the item was removed; false if it was not present.</returns>
+		bool ICollection<T>.Remove(T item)
+		{
+			return Remove(item);
 		}
 
 		/// <summary>
@@ -119,7 +144,17 @@ namespace Grammophone.DataAccess.Collections
 			base.Clear();
 
 			if (items.Count > 0)
+			{
 				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, items));
+			}
+		}
+
+		/// <summary>
+		/// Removes all elements from the set and raises the appropriate collection changed event.
+		/// </summary>
+		void ICollection<T>.Clear()
+		{
+			Clear();
 		}
 
 		/// <summary>
@@ -128,11 +163,30 @@ namespace Grammophone.DataAccess.Collections
 		/// </summary>
 		public new void UnionWith(IEnumerable<T> other)
 		{
-			var added = other.Where(x => !Contains(x)).ToList();
+			if (other == null) throw new ArgumentNullException(nameof(other));
+
+			var added = new List<T>();
+
+			foreach (var item in other)
+			{
+				if (base.Add(item))
+				{
+					added.Add(item);
+				}
+			}
+
 			if (added.Count == 0) return;
 
-			base.UnionWith(added);
 			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, added));
+		}
+
+		/// <summary>
+		/// Modifies the current set to contain all elements that are present in itself,
+		/// the specified collection, or both.
+		/// </summary>
+		void ISet<T>.UnionWith(IEnumerable<T> other)
+		{
+			UnionWith(other);
 		}
 
 		/// <summary>
@@ -141,11 +195,27 @@ namespace Grammophone.DataAccess.Collections
 		/// </summary>
 		public new void ExceptWith(IEnumerable<T> other)
 		{
-			var removed = other.Where(Contains).ToList();
+			if (other == null) throw new ArgumentNullException(nameof(other));
+
+			var otherSet = new HashSet<T>(other, Comparer);
+			var removed = this.Where(otherSet.Contains).ToList();
+
 			if (removed.Count == 0) return;
 
-			base.ExceptWith(removed);
+			foreach (var item in removed)
+			{
+				base.Remove(item);
+			}
+
 			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, removed));
+		}
+
+		/// <summary>
+		/// Removes all elements in the specified collection from the current set.
+		/// </summary>
+		void ISet<T>.ExceptWith(IEnumerable<T> other)
+		{
+			ExceptWith(other);
 		}
 
 		/// <summary>
@@ -154,11 +224,96 @@ namespace Grammophone.DataAccess.Collections
 		/// </summary>
 		public new void IntersectWith(IEnumerable<T> other)
 		{
-			var toRemove = this.Except(other).ToList();
+			if (other == null) throw new ArgumentNullException(nameof(other));
+
+			var otherSet = new HashSet<T>(other, Comparer);
+			var toRemove = this.Where(item => !otherSet.Contains(item)).ToList();
+
 			if (toRemove.Count == 0) return;
 
-			base.IntersectWith(other);
+			foreach (var item in toRemove)
+			{
+				base.Remove(item);
+			}
+
 			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, toRemove));
+		}
+
+		/// <summary>
+		/// Modifies the current set to contain only elements that are present in both
+		/// the current set and the specified collection.
+		/// </summary>
+		void ISet<T>.IntersectWith(IEnumerable<T> other)
+		{
+			IntersectWith(other);
+		}
+
+		/// <summary>
+		/// Modifies the current set to contain only elements that are present either in the current set
+		/// or in the specified collection, but not both.
+		/// </summary>
+		public new void SymmetricExceptWith(IEnumerable<T> other)
+		{
+			if (other == null) throw new ArgumentNullException(nameof(other));
+
+			var currentSet = new HashSet<T>(this, Comparer);
+			var otherSet = new HashSet<T>(other, Comparer);
+			var removed = currentSet.Where(otherSet.Contains).ToList();
+			var added = otherSet.Where(item => !currentSet.Contains(item)).ToList();
+
+			if (removed.Count == 0 && added.Count == 0) return;
+
+			foreach (var item in removed)
+			{
+				base.Remove(item);
+			}
+
+			foreach (var item in added)
+			{
+				base.Add(item);
+			}
+
+			if (removed.Count > 0)
+			{
+				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, removed));
+			}
+
+			if (added.Count > 0)
+			{
+				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, added));
+			}
+		}
+
+		/// <summary>
+		/// Modifies the current set to contain only elements that are present either in the current set
+		/// or in the specified collection, but not both.
+		/// </summary>
+		void ISet<T>.SymmetricExceptWith(IEnumerable<T> other)
+		{
+			SymmetricExceptWith(other);
+		}
+
+		/// <summary>
+		/// Removes all elements that match the conditions defined by the specified predicate.
+		/// </summary>
+		/// <param name="match">The predicate that defines the conditions of the elements to remove.</param>
+		/// <returns>The number of elements that were removed.</returns>
+		public new int RemoveWhere(Predicate<T> match)
+		{
+			if (match == null) throw new ArgumentNullException(nameof(match));
+
+			var removed = this.Where(item => match(item)).ToList();
+
+			if (removed.Count == 0) return 0;
+
+			foreach (var item in removed)
+			{
+				base.Remove(item);
+			}
+
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, removed));
+
+			return removed.Count;
 		}
 	}
 }
